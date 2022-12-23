cd95465fe5be2563751624d031a8e1431ed9561f
leeyounggil <youngily25@gmail.com>
2022-12-23 14:16:45 +0900

    환경ㄴ세팅 완료

---
+++ src/main/java/com/honors/blog/Blog1121Application.java
@@ -0,0 +1,13 @@
+package com.honors.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blog1121Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Blog1121Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/honors/blog/Blog1121ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.honors.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Blog1121ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
